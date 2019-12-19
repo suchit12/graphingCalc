c81cf8f41d557d58d404fe20665c44a0a808791d
suchit12 <guestbandaram@gmail.com>
2019-12-18 21:33:50 -0800

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+//Essentially the main class of the project, and is what can be referred to to initiate it
+
+public class Calculator {
+    public static void main(String[] args) {
+        new CalculatorView();
+    }
+}
